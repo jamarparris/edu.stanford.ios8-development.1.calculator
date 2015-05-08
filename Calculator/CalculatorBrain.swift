1aa0201ce94b423d4bc519f8237c3fed82d616e3
Jamar Parris <jamar@jamarparris.com>
2015-05-07 22:17:54 -0400

    Added typealias for PropertyList. Added ability to get and set program by converting opStack to and from array of strings.

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -61,6 +61,28 @@ class CalculatorBrain
         }
     }
     
+    typealias PropertyList = AnyObject
+    
+    var program: PropertyList { //guaranteed to be a PropertyList
+        get {
+            return opStack.map { $0.description }
+        }
+        set {
+            if let opSymbols = newValue as? [String] {
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols {
+                    if let op = knownOps[opSymbol] {
+                        newOpStack.append(op)
+                    } else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+                        newOpStack.append(.Operand(operand))
+                    }
+                }
+                
+                opStack = newOpStack
+            }
+        }
+    }
+    
     var description: String {
         get {
             
