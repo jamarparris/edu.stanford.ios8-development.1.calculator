2fad2206d6c05063217d32dec42d015ef7186c00
Jamar Parris <jamar@jamarparris.com>
2015-05-23 10:31:03 -0400

    Added UISplitViewController support and placeholder GraphViewController for future graphing functionality.

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CalculatorViewController.swift
 //  Calculator
 //
 //  Created by Jamar Parris on 3/8/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class CalculatorViewController: UIViewController
 {
     @IBOutlet weak var display: UILabel!
     @IBOutlet weak var history: UILabel!
---
+++ Calculator/GraphViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GraphViewController.swift
+//  Calculator
+//
+//  Created by Jamar Parris on 5/23/15.
+//  Copyright (c) 2015 Jamar Parris. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+
+
+}
